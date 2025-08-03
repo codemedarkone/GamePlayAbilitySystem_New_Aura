910dc1dfe06a157997608ffbee92ef8230d63e9d
chris UN <codemedarkone@gmail.com>
2025-08-03 16:32:26 -0600

    The Base Character Class

--- Source/NewAura/NewAura.Build.cs
+++ Source/NewAura/NewAura.Build.cs
@@ -8,9 +8,9 @@ public class NewAura : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "GameplayTags", "GameplayTasks", "GameplayAbilities" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PrivateDependencyModuleNames.AddRange(new string[] { "NavigationSystem", "Niagara", "AIModule" });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });

74586e65bd68927a65c0eda5ea88a5e848557b39
chris UN <codemedarkone@gmail.com>
2025-08-10 12:20:29 -0600

    Section 5.32: created aurahud class which i added 2 variable to add overlay widget to screen. then created bp aurahud, set the overlaywidget tsubclass of in the blueprint, shows to screen.

--- Source/NewAura/Public/UI/HUD/AuraHUD.h
+++ Source/NewAura/Public/UI/HUD/AuraHUD.h
@@ -6,13 +6,33 @@
 #include "GameFramework/HUD.h"
 #include "AuraHUD.generated.h"
 
+class UAuraUserWidget;
+
 /**
  * 
- * We need a variable to store the overlay widget and the overlay widget class
+ * We need a variable to store the overlay widget and the overlay widget class which we want to spawn
  */
 UCLASS()
 class NEWAURA_API AAuraHUD : public AHUD
 {
 	GENERATED_BODY()
 	
+public:
+
+	//Variable that stores the overlaywidget
+	UPROPERTY()
+	TObjectPtr<UAuraUserWidget> OverlayWidget;
+
+
+protected:
+	// using this temporary to show overlay on the screen
+	virtual void BeginPlay() override;
+
+private:
+
+	//We also need to know the UCLASS, the class of widget to create and spawn
+	//Lets us set it from our hud blueprint(editanywhere)
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<UAuraUserWidget> OverlayWidgetClass;
+
 };
